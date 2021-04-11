221e31bee76e20c27ad7d460713864ba322ade8e
Avnish327030 <anishyadav7045075175@gmail.com>
2021-04-11 10:41:19 +0000

    modeified

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-application=Flask(__name__)
+app=Flask(__name__)
 
-@application.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def index():
     return "<H1>AWS Elastic bean stalk deployment testing</H1>"
 
 if __name__=="__main__":
-    application.run()
+    app.run()
